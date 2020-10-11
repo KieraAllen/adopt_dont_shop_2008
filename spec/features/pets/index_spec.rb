2cbf3a3743acfb6943b7b839c4d4f3c193b11205
Kiera Allen <kieraaallen@gmail.com>
2020-10-11 16:11:27 -0600

    Change shelter name connection to shelter id

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -1,30 +1,45 @@
 RSpec.describe "pets index page", type: :feature do
   it "can see all pets and their attributes" do
-    damion = Pet.create(image: "https://blog.tutorming.com/hs-fs/hubfs/western_dragon.jpg?width=415&name=western_dragon.jpg",
-                        name: "Damion",
-                        approx_age: 2,
-                        sex: "Male",
-                        shelter_name: "Diane's Dragons")
+    dianes_dragons = Shelter.create(name: "Diane's Dragons",
+                               address: "1241 W Bayaud Ave",
+                               city: "Denver",
+                               state: "CO",
+                               zip: "80223"
+                               )
 
-    deirdre = Pet.create(image: "https://upload.wikimedia.org/wikipedia/commons/d/d8/Friedrich-Johann-Justin-Bertuch_Mythical-Creature-Dragon_1806.jpg",
-                        name: "Deirdre",
-                        approx_age: 3,
-                        sex: "Female",
-                        shelter_name: "Diane's Dragons")
+    bernies_beasts = Shelter.create(name: "Bernie's Beasts",
+                               address: "1005 Galapago St",
+                               city: "Denver",
+                               state: "CO",
+                               zip: "80204"
+                               )
 
-    charlie = Pet.create(image: "https://wizardsunitehub.info/wp-content/uploads/2019/04/ui_registry_family10_pg04_sticker01_returned.png",
+    damion = Pet.create!(image: "https://blog.tutorming.com/hs-fs/hubfs/western_dragon.jpg?width=415&name=western_dragon.jpg",
+                         name: "Damion",
+                         approx_age: 2,
+                         sex: "Male",
+                         shelter_id: dianes_dragons.id
+                         )
+
+    deirdre = Pet.create!(image: "https://upload.wikimedia.org/wikipedia/commons/d/d8/Friedrich-Johann-Justin-Bertuch_Mythical-Creature-Dragon_1806.jpg",
+                          name: "Deirdre",
+                          approx_age: 3,
+                          sex: "Female",
+                          shelter_id: dianes_dragons.id
+                          )
+
+    charlie = Pet.create!(image: "https://wizardsunitehub.info/wp-content/uploads/2019/04/ui_registry_family10_pg04_sticker01_returned.png",
                          name: "Charlie",
                          approx_age: 1,
                          sex: "Male",
-                         shelter_name: "Bernie's Beasts")
+                         shelter_id: bernies_beasts.id)
 
     visit "/pets"
 
-    expect(page).to have_content("Name: #{damion.name}")
+    expect(page).to have_content("Name: Damion")
     expect(page).to have_content("Approximate Age: #{damion.approx_age}")
     expect(page).to have_content("Sex: #{damion.sex}")
-    expect(page).to have_content("Shelter Name: #{damion.shelter_name}")
     expect(page).to have_content("Approximate Age: #{deirdre.approx_age}")
-    expect(page).to have_content("Shelter Name: #{charlie.shelter_name}")
+    expect(page).to have_content("Shelter: #{bernies_beasts.name}")
   end
 end
