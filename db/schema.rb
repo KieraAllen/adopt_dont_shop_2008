c0d27a62f7a56467ee9e436ae155ca604b7784b7
Kiera Allen <kieraaallen@gmail.com>
2020-10-11 15:55:10 -0600

    Start User Story 8 and create foreign key relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_11_210101) do
+ActiveRecord::Schema.define(version: 2020_10_11_214409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,9 +20,10 @@ ActiveRecord::Schema.define(version: 2020_10_11_210101) do
     t.string "name"
     t.integer "approx_age"
     t.string "sex"
-    t.string "shelter_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "shelter_id"
+    t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
   create_table "shelters", force: :cascade do |t|
@@ -35,4 +36,5 @@ ActiveRecord::Schema.define(version: 2020_10_11_210101) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "pets", "shelters"
 end
--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -1,16 +1,3 @@
-# User Story 7, Pet Index
-#
-# As a visitor
-# When I visit '/pets'
-# Then I see each Pet in the system including the Pet's:
-# - image
-# - name
-# - approximate age
-# - sex
-# - name of the shelter where the pet is currently located
-#
-# NOTE: Images should be stored as a string as the url pointing to an externally hosted image, for example: "https://dogtime.com/assets/uploads/2018/10/puppies-cover.jpg"
-
 RSpec.describe "pets index page", type: :feature do
   it "can see all pets and their attributes" do
     damion = Pet.create(image: "https://blog.tutorming.com/hs-fs/hubfs/western_dragon.jpg?width=415&name=western_dragon.jpg",
